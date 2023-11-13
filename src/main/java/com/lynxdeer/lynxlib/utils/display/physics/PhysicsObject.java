fdeac70f70f33ebead06f68c563e0d3f482b1c42
Lynxdeer <lynxdeergaming@gmail.com>
2023-11-12 23:04:33 -0500

    A few methods

--- src/main/java/com/lynxdeer/lynxlib/utils/display/physics/PhysicsObject.java
+++ src/main/java/com/lynxdeer/lynxlib/utils/display/physics/PhysicsObject.java
@@ -3,11 +3,22 @@ package com.lynxdeer.lynxlib.utils.display.physics;
 import com.lynxdeer.lynxlib.utils.display.LynxDisplay;
 import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
+import org.joml.Vector3f;
 
 public class PhysicsObject extends LynxDisplay {
 	
+	public Vector3f velocity;
+	
 	public PhysicsObject(Location loc, ItemStack item) {
 		super(loc, item);
+	}
+	
+	public void tick() {
+	
+	}
+	
+	public void applyVelocity(Vector3f vector) {
+	
 	}
 	
 }
